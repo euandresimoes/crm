e06abd77761329db8740b0c5b7c5d7f432d3deec
André Simões <andre.simoesdutra23@gmail.com>
2025-08-01 20:12:18 -0300

    ✨ feat: Add Redis cache to Profile endpoint

--- backend/services/node-auth/src/modules/profile/profile.module.ts
+++ backend/services/node-auth/src/modules/profile/profile.module.ts
@@ -3,8 +3,10 @@ import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
 import { PrismaClient } from 'generated/prisma';
 import { JwtService } from 'src/shared/jwt/jwt.service';
+import { RedisModule } from 'src/shared/redis/redis.module';
 
 @Module({
+  imports: [RedisModule],
   providers: [ProfileService, PrismaClient, JwtService],
   controllers: [ProfileController],
 })
--- backend/services/node-auth/src/modules/profile/profile.service.ts
+++ backend/services/node-auth/src/modules/profile/profile.service.ts
@@ -1,20 +1,35 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { PrismaClient } from 'generated/prisma';
+import { PrismaClient, User } from 'generated/prisma';
 import { ProfileResponseDto } from './profile.dto';
+import Redis from 'ioredis';
 
 @Injectable()
 export class ProfileService {
 
     constructor(
-        private prisma: PrismaClient
+        private prisma: PrismaClient,
+        private redis: Redis
     ) { }
 
     async profile(
         userId: string | number
     ): Promise<ProfileResponseDto> {
+        const userCache = await this.redis.get(`user:${userId}:profile`);
+        if (userCache) {
+            const userParsed = JSON.parse(userCache);
+            return userParsed as ProfileResponseDto;
+        }
+
         const user = await this.prisma.user.findUnique({ where: { id: Number(userId) } });
         if (!user)
             throw new HttpException('Account not found.', HttpStatus.NOT_FOUND);
+
+        await this.redis.set(
+            `user:${user.id}:profile`,
+            JSON.stringify(user),
+            'EX',
+            3600
+        );
 
         return user as ProfileResponseDto;
     }
